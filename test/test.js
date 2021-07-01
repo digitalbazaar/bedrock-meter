ba7d8bf1b74cc1a2bac1cd67ad928e02e76a86bc
David I. Lehn <dlehn@digitalbazaar.com>
2021-07-01 13:44:25 -0400

    Add empty test handlers early.

--- test/test.js
+++ test/test.js
@@ -5,5 +5,15 @@ const bedrock = require('bedrock');
 require('bedrock-express');
 require('bedrock-mongodb');
 
+const {meters} = require('bedrock-meter');
+
+// Handlers need to be added before bedrock.start called.
+// Adding empty handlers here since test files are loaded too late.
+bedrock.events.on('bedrock.init', async () => {
+  meters.setHandler({name: 'insert', handler: () => {}});
+  meters.setHandler({name: 'remove', handler: () => {}});
+  meters.setHandler({name: 'use', handler: () => {}});
+});
+
 require('bedrock-test');
 bedrock.start();
