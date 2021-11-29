09cf7c844f3348aa74dfaf4e688ca2ae942ef511
Josh Dunn <jdunn@digitalbazaar.com>
2021-11-29 12:20:20 -0600

    Add database tests to explain performance.

---
+++ test/mocha/30-database.js
@@ -0,0 +1,64 @@
+/*!
+ * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const {meters} = require('bedrock-meter');
+const {cleanDB} = require('./helpers.js');
+const bnid = require('bnid');
+
+describe.only('Meters Database Tests', function() {
+  let meter1;
+  describe('Indexes', function() {
+    beforeEach(async () => {
+      await cleanDB();
+      meter1 = {id: await bnid.generateId()};
+      const meter2 = {id: await bnid.generateId()};
+
+      // mutliple records are inserted here in order to do proper assertions
+      // for 'nReturned', 'totalKeysExamined' and 'totalDocsExamined'.
+      await meters.insert({meter: meter1});
+      await meters.insert({meter: meter2});
+    });
+    it(`is properly indexed for 'meter.id' and 'meter.sequence' in update()`,
+      async function() {
+        meter1.sequence = 1;
+        const {executionStats} = await meters.update({
+          meter: meter1, explain: true
+        });
+        executionStats.nReturned.should.equal(1);
+        executionStats.totalKeysExamined.should.equal(1);
+        executionStats.totalDocsExamined.should.equal(1);
+        executionStats.executionStages.inputStage.inputStage.stage
+          .should.equal('IXSCAN');
+        executionStats.executionStages.inputStage.inputStage.keyPattern
+          .should.eql({'meter.id': 1});
+      });
+    it(`is properly indexed for 'meter.id' in get()`,
+      async function() {
+        const {executionStats} = await meters.get({
+          id: meter1.id, explain: true
+        });
+        executionStats.nReturned.should.equal(1);
+        executionStats.totalKeysExamined.should.equal(1);
+        executionStats.totalDocsExamined.should.equal(1);
+        executionStats.executionStages.inputStage.inputStage.inputStage.stage
+          .should.equal('IXSCAN');
+        executionStats.executionStages.inputStage.inputStage.inputStage
+          .keyPattern.should.eql({'meter.id': 1});
+      });
+    it(`is properly indexed for 'meter.id' in remove()`,
+      async function() {
+        const {executionStats} = await meters.remove({
+          id: meter1.id, explain: true
+        });
+        executionStats.nReturned.should.equal(1);
+        executionStats.totalKeysExamined.should.equal(1);
+        executionStats.totalDocsExamined.should.equal(1);
+        executionStats.executionStages.inputStage.inputStage.stage
+          .should.equal('IXSCAN');
+        executionStats.executionStages.inputStage.inputStage
+          .keyPattern.should.eql({'meter.id': 1});
+      });
+  });
+});
