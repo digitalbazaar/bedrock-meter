e4eaec1083c062e8b92ed01780d52e5f3ee87612
Dave Longley <dlongley@digitalbazaar.com>
2022-11-04 14:10:10 -0400

    Update meter IDs to use multihash to meet common practice.

--- test/mocha/20-mock-meter.js
+++ test/mocha/20-mock-meter.js
@@ -6,7 +6,7 @@ import {meters} from '@bedrock/meter';
 describe('add mock meter (config.meter.addMockMeters)', () => {
   describe('get mock meter', () => {
     it('should get mock meter', async () => {
-      const id = 'zV2wZh7G61vwMPk2PVuSC1L';
+      const id = 'z19ygjQcNmQ9AbG7hCF39Kizs';
       const {meter} = await meters.get({id});
       should.exist(meter);
       meter.id.should.equal(id);
--- test/mocha/helpers.js
+++ test/mocha/helpers.js
@@ -2,7 +2,17 @@
  * Copyright (c) 2021-2022 Digital Bazaar, Inc. All rights reserved.
  */
 import * as database from '@bedrock/mongodb';
+import {generateId as _generateId} from 'bnid';
 
 export async function cleanDB() {
   await database.collections['meter-meter'].deleteMany({});
+}
+
+export async function generateId() {
+  return _generateId({
+    bitLength: 128,
+    encoding: 'base58',
+    multibase: true,
+    multihash: true
+  });
 }
